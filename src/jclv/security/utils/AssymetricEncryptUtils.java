b09f16ac80f76a042c177a2e36b1acb602f1fa2e
Jean Lambert <jeeaan1702@gmail.com>
2018-06-28 19:47:23 -0700

    encrypt and decrypt functions

---
+++ src/jclv/security/utils/AssymetricEncryptUtils.java
@@ -0,0 +1,25 @@
+package jclv.security.utils;
+import java.nio.charset.StandardCharsets;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+
+public class AssymetricEncryptUtils {
+	
+	public static byte[] Encrypt(PublicKey pk, String message) throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, pk);
+		byte[] encrypted = cipher.doFinal(message.getBytes(StandardCharsets.UTF_8));
+		
+		return encrypted;
+	}
+	
+	public static byte[] Decrypt(PrivateKey pk, byte[] encrypted) throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		cipher.init(Cipher.DECRYPT_MODE, pk);
+		byte[] descrypted = cipher.doFinal(encrypted);
+		
+		return descrypted;
+	}
+}
